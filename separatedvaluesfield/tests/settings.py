95af51c3b5ec1b931e54b8abd1b67d25a6599b39
Florent Messa <florent.messa@gmail.com>
2014-09-16 10:35:01 +0200

    Add compatibility for Django 1.7

--- separatedvaluesfield/tests/settings.py
+++ separatedvaluesfield/tests/settings.py
@@ -22,3 +22,12 @@ SECRET_KEY = 'blabla'
 
 if django.VERSION <= (1, 6):
     TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
